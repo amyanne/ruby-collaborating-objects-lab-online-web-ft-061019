5569d3fee12174357f5ebdac52b0fa1c01731918
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 20:03:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ class Artist
    def self.find_by_name(name)
      self.all.find{|artist| artist.name == name}
    end 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
 end 
