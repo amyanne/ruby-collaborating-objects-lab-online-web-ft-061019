615a01ad774f9831c77f3f58ef6b55db48c365a7
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 20:07:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -34,6 +34,11 @@ class Artist
     self.find_by_name(name) || self.create_by_name(name)
   end
   
-  def prints_songs
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end 
+  end 
+  
     
 end 
