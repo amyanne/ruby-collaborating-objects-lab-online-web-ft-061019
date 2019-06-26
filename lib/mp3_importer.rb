9fc876d7c2d704c04669b758a454239be861fdcd
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 20:14:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,8 +5,9 @@ class MP3Importer
   end 
   
   def files
-    @path
-  end 
+    Dir[@path+"/*.mp3"].map do |file|
+      file.split("/").last
+   end 
     
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
