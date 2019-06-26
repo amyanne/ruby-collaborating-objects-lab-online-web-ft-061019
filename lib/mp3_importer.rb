5654eeab838ab6843125a242f15694bcc57933d7
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 20:13:05 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,6 +3,10 @@ class MP3Importer
   def initialize(path)
     @path = path
   end 
+  
+  def files
+    @path
+  end 
     
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
