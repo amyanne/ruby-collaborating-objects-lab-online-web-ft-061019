7a012701a429f5676d7119988341adc4b4a54899
Amy Westcott <amy.westcott@yahoo.com>
2019-06-26 19:51:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,11 +3,11 @@ class Artist
   
   def initialize(name)
     @name = name 
-    @song = []
+    @songs = []
   end 
   
   def add_song(song)
-    @song << song
+    @songs << song
     song.artist = self
   end 
     
